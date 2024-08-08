55a7e556a856ec25ca16162b970eef333ff706fc
lykiuun56 <102335243+lykiuun56@users.noreply.github.com>
2024-08-08 16:32:33 -0700

    server api

--- vue.config.cjs
+++ vue.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8081', // Proxy API requests to the backend
+        target: 'http://creator-tools.us-east-1.elasticbeanstalk.com', // Proxy API requests to the backend
         changeOrigin: true,
       },
     },
